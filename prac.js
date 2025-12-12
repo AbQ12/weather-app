adb0788a632b4c758b364f893f6dc69bb0034ce8
AbQ12 <abubakar.q098@gmail.com>
2025-12-12 15:21:04 +0500

    little bit changes or addition

--- prac.js
+++ prac.js
@@ -8,18 +8,34 @@ const temperatureDisp = document.getElementById('temperature');
 const descriptionDisp = document.getElementById('description');
 const errorMessageDisp = document.getElementById('error-message');
 
-getInputbtn.addEventListener('click',() => {
+const API_key = '9f8998c1883b23c9bbb13b68c699c8ad';//env variable
+
+// 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric'
+getInputbtn.addEventListener('click',async () => {
     const city = cityInput.value.trim();
-    if(!city){return;}
+    if(!city){return alert("the string is empty!!")}
+    try{
+     await fetchWeatherData(city)
+    }catch{
+        shoeError()
+    }
+
 
 })
 
-function fetchWeatherData(){
+function fetchWeatherData(city){
     //fetches the weather data
+    
+}
+function displayWeatherData(){
+    //displays the weather data
 }
 
 function displayError(){
     //displays the error
+    weatherInfo.classList.add('hidden');
+    errorMessageDisp.classList.remove('hidden');
+
 
 }
 
