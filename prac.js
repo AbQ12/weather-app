8f17db6064ecd35cf8851747e8f1565085d4a006
AbQ12 <abubakar.q098@gmail.com>
2025-12-16 12:25:38 +0500

    new todo list

--- prac.js
+++ prac.js
@@ -10,12 +10,13 @@ const errorMessageDisp = document.getElementById('error-message');
 
 const API_key = '9f8998c1883b23c9bbb13b68c699c8ad';//env variable
 
-// 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric'
+
 getInputbtn.addEventListener('click',async () => {
     const city = cityInput.value.trim();
     if(!city){return alert("the string is empty!!")}
     try{
-     await fetchWeatherData(city)
+     const weatherData = await fetchWeatherData(city);
+     displayWeatherData(weatherData);
     }catch{
         shoeError()
     }
@@ -23,45 +24,33 @@ getInputbtn.addEventListener('click',async () => {
 
 })
 
-function fetchWeatherData(city){
+async function fetchWeatherData(city){
     //fetches the weather data
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric';
+    const response = fetch(url);
+    if(!response.ok){
+        throw new Error('city not found');
+    }
+    const data = (await response).json()
+    return data;
+    
     
 }
-function displayWeatherData(){
+function displayWeatherData(weatherData){
     //displays the weather data
-}
+    console.log(weatherData);
+    const {name,main,weather} = weatherData;
+    cityNameDisp.textContent = name;
+    temperatureDisp.textContent = `temperature : ${main.temperature}`;
+    descriptionDisp.textContent = `weather: ${weather[0].description}`;
+    //unlock disp
+    weatherInfo.classList.remove('hidden');
+    errorMessageDisp.classList.add('hidden');
 
+}
 function displayError(){
     //displays the error
-    weatherInfo.classList.add('hidden');
+    weatherInfo.classList.remove('hidden');
     errorMessageDisp.classList.remove('hidden');
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
